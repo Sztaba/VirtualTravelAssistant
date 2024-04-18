2556b76a9e5a671c006697b6da23f22eec9d87b5
Hubert Asztabski <asztabski@student.agh.edu.pl>
2024-04-18 19:38:13 +0200

    bag of words algorithm completed

--- Processor/algorithms.py
+++ Processor/algorithms.py
@@ -1,11 +1,26 @@
 import basic
+import pandas as pd
 
-sentence = "John likes to watch movies. Mary likes movies too."
 
-def bag_of_words(sentence) -> None:
-    words = basic.preprocessKinds(sentence)
+def bag_of_words(documents: []) -> {}:
     bow = {}
-    for key in words:
-        bow[key] = words.count(key)
+    for i, d in enumerate(documents):
+        words = basic.preprocessKinds(d)
+        for key in words:
+            bow[key] = bow.get(key, [0] * (len(documents) + 1))
+            bow[key][i] += 1
+            bow[key][len(documents)] += 1
+    columns = ['doc' + str(i + 1) for i in range(len(documents))]
+    columns.append('total')
+    bow_df = pd.DataFrame.from_dict(bow, orient='index', columns=columns)
+    return bow_df
 
-bag_of_words(sentence)
+
+doc_contents = []
+file_names = ["doc1.txt", "doc2.txt", "doc3.txt"]
+
+for file_name in file_names:
+    with open("../Data/" + file_name, "r") as file:
+        doc_contents.append(file.read())
+
+print(bag_of_words(doc_contents))
