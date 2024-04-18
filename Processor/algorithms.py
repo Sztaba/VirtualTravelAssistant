818b29cca19e6733f21edb19e40d587ec490e271
Hubert Asztabski <asztabski@student.agh.edu.pl>
2024-04-18 20:10:45 +0200

    LSA/LSI algorithm completed

--- Processor/algorithms.py
+++ Processor/algorithms.py
@@ -1,8 +1,11 @@
 import basic
 import pandas as pd
+from nltk.tokenize import RegexpTokenizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
 
 
-def bag_of_words(documents: []) -> {}:
+def bag_of_words(documents: list[str]) -> pd.DataFrame:
     bow = {}
     for i, d in enumerate(documents):
         words = basic.preprocessKinds(d)
@@ -16,6 +19,27 @@ def bag_of_words(documents: []) -> {}:
     return bow_df
 
 
+def lsa(documents: list[str]) -> {}:
+    tokenizer = RegexpTokenizer(r'\w+')
+    tfidf = TfidfVectorizer(lowercase=True,
+                            stop_words='english',
+                            ngram_range=(1, 1),
+                            tokenizer=tokenizer.tokenize)
+    train_data = tfidf.fit_transform(documents)
+
+    num_components = 5
+    lsa = TruncatedSVD(n_components=num_components, n_iter=100, random_state=42)
+    lsa.fit_transform(train_data)
+    terms = tfidf.get_feature_names_out()
+    lsa_out = {}
+    for index, component in enumerate(lsa.components_):
+        zipped = zip(terms, component)
+        top_terms_key = sorted(zipped, key=lambda t: t[1], reverse=True)[:5]
+        top_terms_list = list(dict(top_terms_key).keys())
+        lsa_out["Topic " + str(index)] = top_terms_list
+    return lsa_out
+
+
 doc_contents = []
 file_names = ["doc1.txt", "doc2.txt", "doc3.txt"]
 
@@ -23,4 +47,5 @@ for file_name in file_names:
     with open("../Data/" + file_name, "r") as file:
         doc_contents.append(file.read())
 
-print(bag_of_words(doc_contents))
+print("\nBag of Words algorithm:\n\n", bag_of_words(doc_contents))
+print("\n\nLatent Semantic Indexing/Analysis algorithm:\n\n", pd.DataFrame(lsa(doc_contents)))
